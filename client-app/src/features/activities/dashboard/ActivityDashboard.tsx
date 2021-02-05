91fcd5b843c355bffb2a8f1c43fb9c2230b6fb82
maria_udalova <maria@t100.group>
2021-02-05 15:43:45 +0300

    Section 5. React - basic layout

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  setSelectedActivity,
+  editMode,
+  setEditMode,
+}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} selectActivity={selectActivity} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+        {editMode && <ActivityForm setEditMode={setEditMode} activity={selectedActivity!} />}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default ActivityDashboard;
